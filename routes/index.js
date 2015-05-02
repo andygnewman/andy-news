ddb756392bf6fda223881fb8199231c9876a142c
Andy <andygnewman@gmail.com>
2015-05-02 18:40:29 +0100

    post by id as param and get routes

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
 
 router.get('/posts', function(req, res, next) {
   Post.find(function(err, posts){
-    if(err){ return next(err); }
+    if (err){ return next(err); }
 
     res.json(posts);
   });
@@ -22,10 +22,26 @@ router.post('/posts', function(req, res, next) {
   var post = new Post(req.body);
 
   post.save(function(err, post) {
-    if(err) { return next(err); }
+    if (err) { return next(err); }
 
     res.json(post);
   });
+});
+
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function(err, post) {
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can not find post')); }
+
+    req.post = post;
+    return next();
+  });
+});
+
+router.get('/posts/:post', function(req, res) {
+  res.json(req.post);
 });
 
 module.exports = router;
