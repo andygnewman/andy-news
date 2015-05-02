81b17be529b908b0a45d40a4a925cd02e178950f
Andy <andygnewman@gmail.com>
2015-05-02 18:51:07 +0100

    post upvote method in schema and routing - not quite working yet

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,7 @@ router.post('/posts', function(req, res, next) {
   });
 });
 
-router.param('post', function(req, res, next, id) {
+router.param('/post', function(req, res, next, id) {
   var query = Post.findById(id);
 
   query.exec(function(err, post) {
@@ -42,6 +42,14 @@ router.param('post', function(req, res, next, id) {
 
 router.get('/posts/:post', function(req, res) {
   res.json(req.post);
+});
+
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
 });
 
 module.exports = router;
