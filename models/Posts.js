45ad27e0778088849d9871cff4a217d490c75d17
Andy <andygnewman@gmail.com>
2015-05-02 18:16:59 +0100

    mongoose schema for Comments created

--- models/Posts.js
+++ models/Posts.js
@@ -6,3 +6,5 @@ var PostSchema = new mongoose.Schema({
   upvotes: {type: number, default: 0},
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
 });
+
+mongoose.model('Post', PostSchema);
