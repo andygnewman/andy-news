5dea485d587a07c9f0fca413e8976deb0f2b5db8
Andy <andygnewman@gmail.com>
2015-05-21 19:48:22 +0100

    enabled persistent saving of posts some unit and e2e tests now failing

--- public/javascripts/services.js
+++ public/javascripts/services.js
@@ -4,15 +4,12 @@ var andyNewsAppServices = angular.module('andyNewsAppServices', []);
 
 
 andyNewsAppServices.factory('postsFactory', ['$http', function($http) {
-  var o = {
-    posts: [
-            {title: 'post1', upvotes: 5, comments: []},
-            {title: 'post2', upvotes: 2, comments: []},
-            {title: 'post3', upvotes: 15, comments: []},
-            {title: 'post4', upvotes: 9, comments: []},
-            {title: 'post5', upvotes: 4, comments: []}
-    ]
-  };
+  var o = {posts: []};
+  o.create = function(post) {
+    return $http.post('/posts', post).success(function(data) {
+      o.posts.push(data);
+    });
+  }
   o.getAll = function() {
     return $http.get('/posts').success(function(data) {
       angular.copy(data, o.posts);
